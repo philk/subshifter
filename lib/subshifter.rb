d2322711ec710cfac278940c0a6fcc347608085f
Phil Kates <me@philkates.com>
2009-10-02 08:58:50 -0400

    Setup autotest Created tests Finished timecode class

--- lib/subshifter.rb
+++ lib/subshifter.rb
@@ -1,9 +1,33 @@
 class Subshifter
-  attr_accessor :shift_time
+  attr_accessor :shift_time, :shift_type 
 
-  def initialize(shift_time)
+  def initialize(shift_time, shift_type, srtfile)
     @shift_time = shift_time
+    @shift_type = shift_type 
+    @srtfile = srtfile
+    sms = @shift_time.split(",")
+    @shift_integer = sms[0] * 60 + sms[1]
   end
 
+  def to_ms
+    
+  end
+
+  def time_shift(timecode)
+    newms = @shift_integer + timecode
+  end
+
+  def time_demux(time)
+    time = time.split(":")
+    s_ms = time[2].split(",")
+    time[2] = s_ms[0]
+    time[3] = s_ms[1]
+    time.map! {|n| n.to_i}
+    return time
+  end
+
+  def time_mux(time)
+    string = "%02d:%02d:%02d,%03d" % time
+  end
 
 end
---
+++ lib/timecode.rb
@@ -0,0 +1,73 @@
+class Timecode
+  attr_accessor :hours, :minutes, :seconds, :milliseconds, :shift
+  def initialize(timecode)
+    @timecode = timecode
+    if @timecode.is_a?(String)
+      @time_array = self.to_array
+    elsif @timecode.is_a?(Array)
+      @time_array = @timecode
+      @timecode = self.to_s
+    end
+    @hours = @time_array[0]
+    @minutes = @time_array[1]
+    @seconds = @time_array[2]
+    @milliseconds = @time_array[3]
+  end
+
+  def to_array
+    colon_split = @timecode.split(":") 
+    comma_split = colon_split[2].split(",")
+    time_array = [colon_split[0], colon_split[1], comma_split[0], comma_split[1]]
+    time_array.map! {|a| a.to_i}
+  end
+
+  def to_s
+    string = "%02d:%02d:%02d,%03d" % @time_array
+  end
+
+  def shift=(shift)
+    @shift = shift_calc(shift)
+  end
+
+  def shift_calc(shift_value)
+    shift_split = shift_value.split(",")
+    shift_split.map! {|a| a.to_i}
+    shift = (shift_split[0] * 1000) + shift_split[1]
+  end
+
+  def shifter()
+    newms = (@seconds * 1000) + @milliseconds + @shift
+    array = [@hours, @minutes, newms / 1000, newms % 1000]
+    if array[2] >= 60
+      div = array[2].divmod(60)
+      array[1] = div[0] + array[1]
+      array[2] = div[1]
+    end
+    if array[1] >= 60
+      div = array[1].divmod(60)
+      array[0] = div[0] + array[0]
+      array[1] = div[1]
+    end
+    if array[2] < 0
+      div = array[2].divmod 60
+      array[1] = div[0] + array[1]
+      array[2] = div[1]
+    end
+    if array[1] < 0
+      div = array[1].divmod 60
+      array[0] = div[0] + array[0]
+      array[1] = div[1]
+    end
+    Timecode.new(array)
+  end
+
+  def shift_forward(input)
+    self.shift = input
+    shifter
+  end
+  def shift_backward(input)
+    self.shift = input
+    @shift = @shift * -1
+    shifter
+  end
+end
