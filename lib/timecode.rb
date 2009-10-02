d94de2931d4475dfdade5b82eeec377cacf6347c
Phil Kates <me@philkates.com>
2009-10-02 10:55:09 -0400

    Moved in jeweler files Created documentation in yard

--- lib/timecode.rb
+++ lib/timecode.rb
@@ -1,5 +1,7 @@
+# This is way overkill for dealing with just a simple string and there's probably a simpler way to deal with some of these issues.
 class Timecode
   attr_accessor :hours, :minutes, :seconds, :milliseconds, :shift
+  # Only requires a timecode, can be either a string or array
   def initialize(timecode)
     @timecode = timecode
     if @timecode.is_a?(String)
@@ -14,6 +16,9 @@ class Timecode
     @milliseconds = @time_array[3]
   end
 
+  # Converts the timecode string into an array
+  #
+  # @return [Array] [hours, minutes, seconds, milliseconds]
   def to_array
     colon_split = @timecode.split(":") 
     comma_split = colon_split[2].split(",")
@@ -21,20 +26,36 @@ class Timecode
     time_array.map! {|a| a.to_i}
   end
 
+  # Outputs the timecode as a string
+  #
+  # @return [String] "hours:minutes:seconds,milliseconds"
   def to_s
     string = "%02d:%02d:%02d,%03d" % @time_array
   end
 
+  # Setter takes a string 
+  # Getter returns an integer
+  #
+  # @param [String] format "ss,mmm" i.e. "seconds,milliseconds"
+  # @return [Integer] seconds * 1000 + milliseconds
   def shift=(shift)
     @shift = shift_calc(shift)
   end
 
+  # Converts the "ss,mmm" shift format into an integer so we can do math on it
+  #
+  # @param [String] format "ss,mmm" i.e. "seconds,milliseconds"
+  # @return [Integer] returns that integer if you need it
   def shift_calc(shift_value)
     shift_split = shift_value.split(",")
     shift_split.map! {|a| a.to_i}
     shift = (shift_split[0] * 1000) + shift_split[1]
   end
 
+  # Shifts the time in a specified direction
+  #
+  # @param [String] either "forward" or "backward"
+  # @return [Timecode] returns a new Timecode object
   def shift_time(direction)
     if direction == "forward"
       shift_forward
@@ -66,14 +87,6 @@ class Timecode
     Timecode.new(array)
   end
 
-  def shift_forward
-    @shift
-  end
-
-  def shift_backward
-    @shift = @shift * -1
-  end
-
   def negative?
     if @time_array.select { |i| i < 0}.empty?
       return false
@@ -81,4 +94,21 @@ class Timecode
       return true
     end
   end
+
+  private
+
+  # Shift time forward
+  #
+  # @return [Integer] just returns the @shift value.  This is completely unnecessarily.
+  def shift_forward
+    @shift
+  end
+
+  # Shift time backward
+  #
+  # @return [Integer] turns @shift negative.
+  def shift_backward
+    @shift = @shift * -1
+  end
+
 end
--- spec/timecode_spec.rb
+++ spec/timecode_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../lib/timecode.rb'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Timecode do
   before(:all) do
